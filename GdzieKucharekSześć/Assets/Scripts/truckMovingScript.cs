79d5095b8411493168a41e251ee3ac6859d38646
Bartosz Bień <bartekb340@gmail.com>
2020-03-22 14:45:58 +0100

    truck moving
    Trzeba stworzyc gameobject "dostawy" i tam wlozyc ten skrpt. Potem przeciagnac ciezarowke w skrypt. I zrobic rotation z 180, bo jedzie z dolu do gory

---
+++ GdzieKucharekSześć/Assets/Scripts/truckMovingScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class truckMovingScript : MonoBehaviour
+{
+    public GameObject truck;
+    private float speed = 1.7f;
+    bool ifComes = false;
+    bool ifGoes = false;
+    
+    public void truckMoveToRestaurant()
+    {
+        ifComes = true;
+    }
+
+    public void truckGoFromRestaurant()
+    {
+        ifGoes = true;
+    }
+
+    void Update()
+    {
+        if (ifComes)
+        {
+            Vector3 move = new Vector3(0, 1, 0);
+            truck.transform.Translate(move * speed * Time.deltaTime);
+
+            if (truck.transform.position[1] > 3)
+                ifComes = false;
+        }
+
+        if (ifGoes)
+        {
+            Vector3 move = new Vector3(0, 1, 0);
+            truck.transform.Translate(move * speed * Time.deltaTime);
+
+            if (truck.transform.position[1] > 20)
+            {
+                ifGoes = false;
+                truck.transform.position = new Vector3(-15.5f, -15.0f, 0);
+            }
+                
+        }
+
+    }
+}
