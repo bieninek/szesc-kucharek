1af41f58507d4c4a778c3ac4fafcc9fcf1608ceb
Bartosz Bień <bartekb340@gmail.com>
2020-03-22 14:53:25 +0100

    stats added

---
+++ GdzieKucharekSześć/Assets/Scripts/stats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stats : MonoBehaviour
+{
+
+    public int cake, pineapple, sausage, cheese, tomatoe, mushroom, mozzarrella, spices;
+
+    void Start()
+    {
+        cake = 3;
+        pineapple = 2;
+        sausage = 2;
+        cheese = 3;
+        tomatoe = 4;
+        mushroom = 2;
+        mozzarrella = 5;
+        spices = 6;
+    }
+
+}
